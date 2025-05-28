fc473d6fbce5274cb7f2b3e2615081f87ca32bf6
Dinesh-Johnson <dinesh.xworkz@gmail.com>
2025-05-28 10:51:20 +0530

    27-May-2025-Assignment

---
+++ MethodChaining/src/com/xworz/method/SocialMedia.java
@@ -0,0 +1,33 @@
+package com.xworz.method;
+
+public class SocialMedia {
+
+    public SocialMedia(){
+        this("emailid@gmail.com");
+        System.out.println("..Social Media Log in..");
+        System.out.println("*******************");
+    }
+    public SocialMedia(String userID){
+        System.out.println("Username/mail :"+userID);
+        System.out.println();
+    }
+    public void post(){
+        post("Love");
+        System.out.println("Create a Post");
+        System.out.println();
+    }
+    public void post(String reactions){
+        System.out.println("Notification");
+        System.out.println("Your has react with :"+reactions);
+        System.out.println();
+    }
+    public static void friends(){
+        friends("Memes",16000);
+        System.out.println("Profile Details");
+        System.out.println();
+    }
+    public static void friends(String pageName,int followers){
+        System.out.println("Page name :"+pageName);
+        System.out.println("Followers :"+followers+"--Followers");
+    }
+}
